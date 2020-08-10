2f8bd8e979d9b994f379599b6817e6c110f4620f
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-08-10 14:03:11 -0700

    imported and rendered todoitem component in Todo

---
+++ test/src/components/TodoItem.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<p>Todo Item</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name:"TodoItem"
+}
+</script>
+
+<style scoped>
+
+.todo-item {
+	background: #f4f4f4;
+	padding: 10px;
+	border-bottom: 1px #ccc dotted;
+}
+
+.is-complete {
+	text-decoration: like-through;
+}
+
+.del {
+	background: #ff0000;
+	color: #fff;
+	border: none;
+	padiing: 5px 9px;
+	border-radius: 50%;
+	cursor: pointer;
+	float: right;
+}
+
+</style>
--- test/src/components/Todos.vue
+++ test/src/components/Todos.vue
@@ -1,15 +1,19 @@
-/* eslint-disable vue/require-v-for-key */
 <template>
 	<div>
 		<div v-for="todo in todos" v-bind:key="todo.id">
-			<h3>{{todo.title}}</h3>
+			<TodoItem />
 		</div>
 	</div>
 </template>
 
 <script>
+import TodoItem from './TodoItem'
+
 export default {
 	name :"Todos",
+	components: {
+		TodoItem
+	},
 	props: ["todos"]
 }
 </script>
