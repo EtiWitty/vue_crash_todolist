ea289b691675df39d3693039b9c40cbb861330b4
EtiWitty <atiyeh.keshavarz@gmail.com>
2020-08-10 13:20:33 -0700

    store data in the main app

--- test/src/App.vue
+++ test/src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id:1,
+          title:" Todo One",
+          completed: false
+        },
+        {
+          id:2,
+          title:" Todo Two",
+          completed: true
+        },
+        {
+          id:3,
+          title:" Todo Three",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    box-sizing: border-bos;
+    margin:0;
+    padding: 0;
+  }
+
+  body {
+    font-family: Arial, Helvetica, sans-serif;
+    line-height: 1.4;
+  }
 </style>
